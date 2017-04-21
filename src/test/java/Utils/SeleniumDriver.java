5642c0f95d09f7cfe190f79e63b44e99e8d67a93
siddharthasingh <siddhartha.3@tcs.com>
2017-04-21 11:21:26 +0530

    Update SeleniumDriver.java

--- src/test/java/Utils/SeleniumDriver.java
+++ src/test/java/Utils/SeleniumDriver.java
@@ -24,7 +24,7 @@ public class SeleniumDriver {
 	public void beforeClass() {
 
 		// driver = new FirefoxDriver();
-		System.setProperty("webdriver.chrome.driver", "/Users/Shared/Jenkins/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/tmp/chromedriver");
 	        WebDriver driver = new ChromeDriver();
 		
 		driver.get("http://ec2-52-36-155-174.us-west-2.compute.amazonaws.com:8086/Ewallet_devops-0.0.1/");
